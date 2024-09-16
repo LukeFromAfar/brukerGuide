0a57b7e04e2795866f8d4733501241690ec360f9
Lukasz <lukasz.gamvik@gmail.com>
2024-09-16 13:08:41 +0200

    added signup

--- server.js
+++ server.js
@@ -17,6 +17,22 @@ app.get("/login", (req, res) => {
     res.render("login")
 })
 
+app.post("/login", (req, res) => {
+    console.log("logger ut her", req.body);
+    const {uname, psw} = req.body;
+    console.log(uname);
+})
+
+app.get("/signup", (req, res) => {
+    res.render("signup")
+})
+
+app.post("/signup", (req, res) => {
+    console.log("logger ut her", req.body);
+    const {uname, psw} = req.body;
+    console.log(uname);
+})
+
 app.get("/guide", (req, res) => {
     res.render("guide")
 })
