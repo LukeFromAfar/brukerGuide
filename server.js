7f17b019d16146033bb0c5aa0890b045a04b5958
Lukasz <lukasz.gamvik@gmail.com>
2024-09-26 11:37:47 +0200

    took inspiration from Monicas work

--- server.js
+++ server.js
@@ -17,16 +17,16 @@ const diskStorage = multer.diskStorage({
         const ext = path.extname(file.originalname);
         console.log("EXT", ext);
 
-        if(ext !== ".png") {
-            return cb(new Error ("Only PNG files allowed"))
-        }
+        // if(ext !== ".png") {
+        //     return cb(new Error ("Only PNG files allowed"))
+        // }
         const fileName = file.originalname + ".png";
         cb(null, fileName)
     }
 });
 
 const uploads = multer(
-    storeage = diskStorage
+    storage = diskStorage
 );
 
 app.set("view engine", "ejs");
@@ -41,20 +41,13 @@ mongoose.connect("mongodb://127.0.0.1:27017/userGuide")
 
 const saltRounds = 10;
 
-const userSchema = new Schema({
-    email: String,
-    password: String
-})
-
-const User = mongoose.model("User", userSchema);
-
 app.get("/", (req, res) => {
     res.render("index")
-})
+});
 
 app.get("/login", (req, res) => {
     res.render("login")
-})
+});
 
 app.post("/login", (req, res) => {
     console.log("logger ut her", req.body);
@@ -79,10 +72,25 @@ app.get("/signup", (req, res) => {
     res.render("signup")
 })
 
+const userSchema = new Schema({
+    email: String,
+    password: String
+})
+
+const guideSchema = new Schema ({
+    title: String,
+    tag: String,
+    header: Array, 
+    description: Array,    
+    image: Array,
+})
+
+const User = mongoose.model("User", userSchema);
+const Guides = mongoose.model("Guides", guideSchema);
 app.post("/signup", async (req, res) => {
     console.log("signer opp her", req.body);
     const {uname, psw, psw2} = req.body;
-    console.log(uname);
+    console.log(uname, psw, psw2, psw == psw2);
 
     if(psw == psw2){
 
@@ -104,30 +112,26 @@ app.post("/signup", async (req, res) => {
 
 app.get("/guide", (req, res) => {
     res.render("guide")
-})
+});
 
 app.get("/dashboard", (req, res) => {
     res.render("dashboard")
-})
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'uploads/');  
-    },
-    filename: (req, file, cb) => {
-      cb(null, Date.now() + path.extname(file.originalname));  
-    }
 });
-  
-const upload = multer({ storage });
 
 app.get("/newGuide", (req, res) => {
-    res.render("newguide");
+    res.render("newGuide");
 });
 
-app.post("/newGuide", uploads.array("image"), (req, res) => {
+app.post("/newGuide", uploads.array("image"), async (req, res) => {
     console.log(req.body, "BODY")
-    console.log(req.file, "FILE")
+    console.log(req.files, "FILE")
+
+    const newGuides = new Guides({ 
+        title: req.body.title, 
+        tag: req.body.tag,
+         header: req.body.header, 
+         description: req.body.description })
+    const result = await newGuides.save();
 });
 
 app.listen(process.env.PORT);
